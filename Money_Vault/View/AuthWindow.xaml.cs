525ac3d710f43f9b02df21e7a09c602adc47a4a0
Xaristel <20muxa00@bk.ru>
2022-01-18 00:22:59 +0700

    update AuthWindow.xaml && add Style.xaml && add Fonts && add Icons

--- Money_Vault/View/AuthWindow.xaml.cs
+++ Money_Vault/View/AuthWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Money_Vault
+namespace Money_Vault.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для AuthWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : Window
     {
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
         }
