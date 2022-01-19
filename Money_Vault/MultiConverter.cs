2cbc1c07539a6346eabbba129f4eecf7a99746fe
Xaristel <20muxa00@bk.ru>
2022-01-20 01:39:32 +0700

    add CommandRelay.cs && update AuthWindow.xaml && AuthViewModel.cs

---
+++ Money_Vault/MultiConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Money_Vault
+{
+    public class MultiConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Tuple<string, string> tuple = new Tuple<string, string>((string)values[0], (string)values[1]);
+            return tuple;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Money_Vault/ViewModel/AuthViewModel.cs
+++ Money_Vault/ViewModel/AuthViewModel.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Money_Vault.Database;
+using Money_Vault.Model;
+using Money_Vault.Properties;
 
-namespace Money_Vault.ViewModel
+namespace Money_Vault.ViewModel 
 {
-    internal class AuthViewModel
+    /// <summary>
+    /// ViewModel для AuthWindow.xaml. Содержит взаимодействие с БД (таблица User)
+    /// </summary>
+    internal class AuthViewModel : INotifyPropertyChanged
     {
+        private DatabaseContext _database;
+
+        private RelayCommand _registerCommand;
+        private RelayCommand _authCommand;
+
+        private bool _isKeepAuthData;
+
+        private IEnumerable<User> _users;
+
+        public IEnumerable<User> Users 
+        { 
+            get => _users;
+            set 
+            {
+                _users = value;
+                OnPropertyChanged("Users");
+            } 
+        }
+
+        public RelayCommand RegisterCommand 
+        { 
+            get => _registerCommand; 
+        }
+
+        public RelayCommand AuthCommand 
+        { 
+            get
+            {
+                return _authCommand ??
+                (_authCommand = new RelayCommand(tuple =>
+                {
+                    string login = ((Tuple<string, string>)tuple).Item1;
+                    string password = ((Tuple<string, string>)tuple).Item2;
+                    Debug.WriteLine(login + " abc " + password);
+                }));
+            }
+        }
+
+        public AuthViewModel()
+        {
+            _database = new DatabaseContext();
+            Users = _database.User.ToList();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
     }
 }
