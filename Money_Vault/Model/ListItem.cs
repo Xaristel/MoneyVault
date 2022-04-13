ab6fdc336279a9e2817f5d0bf6540e321d2e2229
Xaristel <20muxa00@bk.ru>
2022-04-13 20:10:30 +0700

    add test data to Database && add styles for comboboxes and dataGridHeaders && update GeneralPage

---
+++ Money_Vault/Model/ListItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Money_Vault.ViewModel
+{
+    public class ListItem
+    {
+        private string _typeName;
+        private string _totalAmount;
+
+        public string TypeName
+        {
+            get => _typeName;
+            set
+            {
+                _typeName = value;
+            }
+        }
+        public string TotalAmount
+        {
+            get => _totalAmount;
+            set
+            {
+                _totalAmount = value;
+            }
+        }
+    }
+}
--- Money_Vault/ViewModel/GeneralViewModel.cs
+++ Money_Vault/ViewModel/GeneralViewModel.cs
@@ -1,12 +1,362 @@
-﻿using System;
+﻿using Money_Vault.Database;
+using Money_Vault.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Money_Vault.ViewModel
 {
-    internal class GeneralViewModel
+    public class GeneralViewModel : BaseViewModel
     {
+        private DatabaseContext _database;
+
+        private RelayCommand _updateDataCommand;
+
+        private IEnumerable<Income_Type> _income_Types;
+        private IEnumerable<Expense_Type> _expense_Types;
+        private IEnumerable<Income> _incomes;
+        private IEnumerable<Expense> _expenses;
+        private IEnumerable<Account> _accounts;
+        private IEnumerable<ListItem> _incomesList;
+        private IEnumerable<ListItem> _expensesList;
+
+        private List<string> _typesList;
+        private List<string> _yearsList;
+        private string _currentYear;
+        private string _currentMonth;
+        private List<string> _monthsList = new List<string>
+        {
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+        "Полный год"
+        };
+
+        public string CurrentYear
+        {
+            get => _currentYear;
+            set
+            {
+                _currentYear = value;
+                OnPropertyChanged("CurrentYear");
+
+                UpdateData();
+            }
+        }
+
+        public string CurrentMonth
+        {
+            get => _currentMonth;
+            set
+            {
+                _currentMonth = value;
+                OnPropertyChanged("CurrentMonth");
+
+                UpdateData();
+            }
+        }
+
+        public IEnumerable<Income_Type> Income_Types
+        {
+            get => _income_Types;
+            set
+            {
+                _income_Types = value;
+                OnPropertyChanged("Income_Types");
+            }
+        }
+
+        public IEnumerable<Expense_Type> Expense_Types
+        {
+            get => _expense_Types;
+            set
+            {
+                _expense_Types = value;
+                OnPropertyChanged("Expense_Types");
+            }
+        }
+
+        public IEnumerable<Income> Incomes
+        {
+            get => _incomes;
+            set
+            {
+                _incomes = value;
+                OnPropertyChanged("Incomes");
+            }
+        }
+
+        public IEnumerable<Expense> Expenses
+        {
+            get => _expenses;
+            set
+            {
+                _expenses = value;
+                OnPropertyChanged("Expenses");
+            }
+        }
+
+        public IEnumerable<Account> Accounts
+        {
+            get => _accounts;
+            set
+            {
+                _accounts = value;
+                OnPropertyChanged("Accounts");
+            }
+        }
+
+        public List<string> TypesList
+        {
+            get => _typesList;
+            set
+            {
+                _typesList = value;
+                OnPropertyChanged("TypesList");
+            }
+        }
+
+        public List<string> YearsList
+        {
+            get => _yearsList;
+            set
+            {
+                _yearsList = value;
+                OnPropertyChanged("YearsList");
+            }
+        }
+
+        public List<string> MonthsList
+        {
+            get => _monthsList;
+            set
+            {
+                _monthsList = value;
+                OnPropertyChanged("MonthList");
+            }
+        }
+
+        public IEnumerable<ListItem> IncomesList
+        {
+            get => _incomesList;
+            set
+            {
+                _incomesList = value;
+                OnPropertyChanged("IncomesList");
+            }
+        }
+
+        public IEnumerable<ListItem> ExpensesList
+        {
+            get => _expensesList;
+            set
+            {
+                _expensesList = value;
+                OnPropertyChanged("ExpensesList");
+            }
+        }
+
+        //public RelayCommand UpdateDataCommand
+        //{
+        //    get
+        //    {
+        //        return _updateDataCommand ?? (_updateDataCommand = new RelayCommand((args) =>
+        //        {
+        //            UpdateData();
+        //        }));
+        //    }
+        //}
+
+        public void UpdateData()
+        {
+            if (CurrentYear == "Все года" && CurrentMonth != "Полный год")
+            {
+                CurrentMonth = "Полный год";
+            }
+
+            FillIncomesList();
+            FillExpensesList();
+        }
+
+        public GeneralViewModel()
+        {
+            _database = new DatabaseContext();
+
+            Income_Types = _database.Income_Types.ToList();
+            Expense_Types = _database.Expense_Types.ToList();
+            Incomes = _database.Incomes.ToList();
+            Expenses = _database.Expenses.ToList();
+            Accounts = _database.Accounts.ToList();
+
+            FillTypesList();
+            FillYearsList();
+
+            CurrentMonth = MonthsList.ToList()[System.DateTime.Now.Month - 1];
+            CurrentYear = Convert.ToString(System.DateTime.Now.Year);
+
+            UpdateData();
+        }
+
+        private void FillTypesList()
+        {
+            TypesList = new List<string>();
+
+            foreach (var item in Income_Types)
+            {
+                TypesList.Add(item.Name);
+            }
+
+            foreach (var item in Expense_Types)
+            {
+                TypesList.Add(item.Name);
+            }
+        }
+
+        private void FillYearsList()
+        {
+            YearsList = new List<string>
+            {
+                _currentYear
+            };
+
+            foreach (var item in Incomes)
+            {
+                //try to get year from date (14.05.2022 -> 2022)
+                string tempYear = item.Date.Split('.')[2];
+
+                if (!YearsList.Contains(tempYear))
+                {
+                    YearsList.Add(tempYear);
+                }
+            }
+
+            foreach (var item in Expenses)
+            {
+                //try to get year from date (14.05.2022 -> 2022)
+                string tempYear = item.Date.Split('.')[2];
+
+                if (!YearsList.Contains(tempYear))
+                {
+                    YearsList.Add(tempYear);
+                }
+            }
+
+            YearsList.Sort();
+            YearsList.Add("Все года");
+        }
+
+        private void FillIncomesList()
+        {
+            var query = from income in Incomes
+                        group income by income.Income_Type_Id into incomeListItem
+                        select new
+                        {
+                            TypeId = incomeListItem.Key,
+                            TotalAmount = (from item in incomeListItem
+                                           where item.Date.Contains($".{MonthsList.IndexOf(CurrentMonth)}.{CurrentYear}")
+                                           || item.Date.Contains($".0{MonthsList.IndexOf(CurrentMonth)}.{CurrentYear}")
+                                           || (CurrentMonth == "Полный год" && item.Date.Contains($".{CurrentYear}"))
+                                           || CurrentYear == "Все года"
+                                           select item.Total_Amount).Sum()
+                        };
+
+            List<ListItem> tempList = new List<ListItem>();
+
+            int totalSum = 0;
+            int forecast = 0;
+
+            foreach (var item in query)
+            {
+                tempList.Add(new ListItem()
+                {
+                    TypeName = Income_Types.ToList().Find(x => x.Id == item.TypeId).Name,
+                    TotalAmount = item.TotalAmount.ToString()
+                });
+
+                totalSum += item.TotalAmount;
+            }
+
+            tempList.Add(new ListItem()
+            {
+                TypeName = "Итого",
+                TotalAmount = Convert.ToString(totalSum)
+            });
+
+            tempList.Add(new ListItem()
+            {
+                TypeName = "Прогноз",
+                TotalAmount = Convert.ToString(forecast)
+            });
+
+            tempList.Add(new ListItem()
+            {
+                TypeName = "Разница",
+                TotalAmount = Convert.ToString(totalSum - forecast)
+            });
+
+            IncomesList = tempList;
+        }
+
+        private void FillExpensesList()
+        {
+            var query = from expense in Expenses
+                        group expense by expense.Expense_Type_Id into expenseListItem
+                        select new
+                        {
+                            TypeId = expenseListItem.Key,
+                            TotalAmount = (from item in expenseListItem
+                                           where item.Date.Contains($".{MonthsList.IndexOf(CurrentMonth)}.{CurrentYear}")
+                                           || item.Date.Contains($".0{MonthsList.IndexOf(CurrentMonth)}.{CurrentYear}")
+                                           || (CurrentMonth == "Полный год" && item.Date.Contains($".{CurrentYear}"))
+                                           || CurrentYear == "Все года"
+                                           select item.Total_Price).Sum()
+                        };
+
+            List<ListItem> tempList = new List<ListItem>();
+
+            int totalSum = 0;
+            int forecast = 0;
+
+            foreach (var item in query)
+            {
+                tempList.Add(new ListItem()
+                {
+                    TypeName = Expense_Types.ToList().Find(x => x.Id == item.TypeId).Name,
+                    TotalAmount = item.TotalAmount.ToString()
+                });
+            }
+
+            tempList.Add(new ListItem()
+            {
+                TypeName = "Итого",
+                TotalAmount = Convert.ToString(totalSum)
+            });
+
+            tempList.Add(new ListItem()
+            {
+                TypeName = "Прогноз",
+                TotalAmount = Convert.ToString(forecast)
+            });
+
+            tempList.Add(new ListItem()
+            {
+                TypeName = "Разница",
+                TotalAmount = Convert.ToString(totalSum - forecast)
+            });
+
+            ExpensesList = tempList;
+        }
     }
 }
